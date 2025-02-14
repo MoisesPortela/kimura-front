d9b888eead6db5cb92f55a3b2c386e5302ec4da2
Moisés <msportela2@hotmail.com>
2025-02-14 18:31:56 -0300

    Contruir formulario

--- src/app/components/form-cadastro/form-cadastro.component.ts
+++ src/app/components/form-cadastro/form-cadastro.component.ts
@@ -1,11 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { PessoaListar } from '../../models/Pessoa';
 
 @Component({
   selector: 'app-form-cadastro',
-  imports: [],
+  imports: [FormsModule, RouterModule, ReactiveFormsModule],
   templateUrl: './form-cadastro.component.html',
-  styleUrl: './form-cadastro.component.css'
+  styleUrl: './form-cadastro.component.css',
 })
-export class FormCadastroComponent {
+export class FormCadastroComponent implements OnInit {
+  @Output()
+  onSubmit = new EventEmitter<PessoaListar>();
 
+  formularioPessoa!: FormGroup;
+
+  constructor() {}
+
+  ngOnInit() {
+    this.formularioPessoa = new FormGroup({
+      id: new FormControl(0),
+      nome: new FormControl(''),
+      cpf: new FormControl(''),
+      rg: new FormControl(''),
+      email: new FormControl(''),
+      idade: new FormControl(''),
+      telefone: new FormControl(''),
+    });
+  }
+  submit() {
+    this.onSubmit.emit(this.formularioPessoa.value);
+  }
 }
