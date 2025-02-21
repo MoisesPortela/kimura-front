999851e07cd73bdc2815cc327fd15a4cef27af99
Moisés <msportela2@hotmail.com>
2025-02-20 22:59:31 -0300

    add: user service implementation

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,9 +1,37 @@
 import { Injectable } from '@angular/core';
-
+import { TokenService } from './token.service';
+import { BehaviorSubject } from 'rxjs';
+import { Usuario } from '../models/Usuario';
+import { jwtDecode } from 'jwt-decode';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
+  private userSubject = new BehaviorSubject<Usuario | null>(null);
 
-  constructor() { }
+  constructor(private tokenService: TokenService) {
+    if (this.tokenService.possuiToken()) {
+      this.decodificarJWT();
+    }
+  }
+  decodificarJWT() {
+    const token = this.tokenService.retornarToken();
+    const user = jwtDecode(token) as Usuario;
+    this.userSubject.next(user);
+  }
+  retornarUser() {
+    return this.userSubject.asObservable();
+  }
+
+  salvarToken(token: string) {
+    this.tokenService.salvarToken(token);
+    this.decodificarJWT();
+  }
+  logout() {
+    this.tokenService.excluirToken();
+    this.userSubject.next(null);
+  }
+  isLogado() {
+    return this.tokenService.possuiToken();
+  }
 }
