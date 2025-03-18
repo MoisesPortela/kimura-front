f3475e0ad8c42d18c0eea29b31838c0fba0724ad
Moisés <msportela2@hotmail.com>
2025-03-17 21:17:02 -0300

    Criação tela "criar conta"

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,9 +4,11 @@ import { HomeComponent } from './pages/home/home.component';
 import { EditarComponent } from './pages/editar/editar.component';
 import { DetalhesComponent } from './pages/detalhes/detalhes.component';
 import { LoginComponent } from './pages/login/login.component';
+import { CriarUsuarioComponent } from './pages/criar-usuario/criar-usuario.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'criarconta', component: CriarUsuarioComponent },
   { path: 'login', component: LoginComponent },
   { path: 'home', component: HomeComponent },
   { path: 'cadastro', component: CadastroComponent },
