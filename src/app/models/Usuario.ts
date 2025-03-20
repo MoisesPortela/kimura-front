52e268fb0a73fad5a426186bc0d5d92ac71c4aa5
Moisés <msportela2@hotmail.com>
2025-03-20 00:06:29 -0300

    add: Create user interface

--- src/app/models/Usuario.ts
+++ src/app/models/Usuario.ts
@@ -3,4 +3,10 @@ export interface Usuario {
   senha: string;
   telefone?: string;
   tipoPerfil?: string;
+  pessoaId?: number;
+  pessoaNome?: string;
+  empresaId?: number;
+  empresaNome?: string;
+  admId?: number;
+  admNome?: string;
 }
--- src/app/pages/criar-usuario/criar-usuario.component.ts
+++ src/app/pages/criar-usuario/criar-usuario.component.ts
@@ -1,11 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatButtonModule } from '@angular/material/button';
+import { Usuario } from '../../models/Usuario';
+import { Router } from '@angular/router';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { CommonModule } from '@angular/common';
+
+interface Roles {
+  id: number;
+  nome: string;
+  valor: string;
+}
 
 @Component({
   selector: 'app-criar-usuario',
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    MatIconModule,
+    MatDividerModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    CommonModule,
+  ],
   templateUrl: './criar-usuario.component.html',
-  styleUrl: './criar-usuario.component.css'
+  styleUrl: './criar-usuario.component.css',
 })
-export class CriarUsuarioComponent {
-
+export class CriarUsuarioComponent implements OnInit {
+  roles: Roles[] = [
+    { id: 1, nome: 'Administrador', valor: 'ROLE_ADMIN' },
+    { id: 2, nome: 'Empresa', valor: 'ROLE_EMPRESA' },
+    { id: 3, nome: 'Empregado', valor: 'ROLE_PESSOA' },
+  ];
+  usuario!: Usuario;
+  userForm!: FormGroup;
+  constructor(private formBuilder: FormBuilder, private router: Router) {}
+  ngOnInit(): void {
+    this.userForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      tipoPerfil: ['', Validators.required], // Valor padrão vazio
+    });
+  }
+  criarUser() {
+    alert('Usuário criado com sucesso!');
+  }
+  voltar() {
+    this.router.navigate(['/']);
+  }
 }
